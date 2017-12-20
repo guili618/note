a20f65a0595c527676448ce414162b030ed7eee0
guili618 <guili618@gmail.com>
2017-12-20 17:31:30 +0800

    20171220

--- python/excel_axample.py
+++ python/excel_axample.py
@@ -2,7 +2,7 @@ import openpyxl
 wb = openpyxl.load_workbook('D:\\f.xlsx')
 sheet = wb.get_active_sheet()
 for i in range(1, 358):
-    sheet.cell(row=i, column=3).value = "insert into sendsms  (smsindex,Phonenumber,Smscontent,Smstime,Smsuser,Status) values( SEQ_SENDSMS.nextval ," \
+    sheet.cell(row=i, column=3).value = "insert into sendsms (smsindex,Phonenumber,Smscontent,Smstime,Smsuser,Status) values( SEQ_SENDSMS.nextval ," \
         + "\'" + str(sheet.cell(row=i, column=1).value) + "\'" + "," \
         + "'内容。'" \
         + ",sysdate,1,0)"
