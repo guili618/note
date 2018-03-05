24663ab139d96071b748692402c3ca99422f6939
guili618 <guili618@gmail.com>
2018-03-05 19:29:42 +0800

    20180305

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,3 +1,23 @@
 var s = 6
-var l = 12
-print（）
+var l = '12'
+console.log(s + l)
+
+function add(a, b) {
+    return a + b;
+
+}
+console.log(add(3, 4))
+Array a1 = [1, 2, 3, 4]
+
+function createNewPerson(name) {
+    var obj = {};
+    obj.name = name;
+    obj.greeting = function() {
+        console.log('Hi! I\'m ' + this.name + '.');
+    };
+    return obj;
+}
+
+var salva = createNewPerson('Salva');
+console.log(salva.name);
+salva.greeting();
