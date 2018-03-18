47e440ec1ce9655b28a6a692d0b7aeae4bfb3dc5
guili618 <guili618@gmail.com>
2018-03-18 19:41:27 +0800

    20180318

--- go/go_in_practices.go
+++ go/go_in_practices.go
@@ -7,7 +7,10 @@ var msg  = `smt`
 var char = 's'
 func main() {
 	fmt.Println("Hello," + "world "+string(char))
-	fmt.Println(string(msg[0])
+	fmt.Println(msg[0])
+	fmt.Println(string("Hello"[1]))              // ASCII only
+    fmt.Println(string([]rune("Hello, 世界")[1])) // UTF-8
+    fmt.Println(string([]rune("Hello, 世界")[8])) // UTF-8
 }
 
 
